4fc56c321141b60afcf12a73306f49a7eb7c241e
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2025-10-14 09:12:41 +0900

    add: 端末を縦に固定をしました

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutterkaigi_2025_booth_game/title_view.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(const MyApp());
 }
 
