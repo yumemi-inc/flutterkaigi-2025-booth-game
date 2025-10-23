11dfc1439405db7139ae0ed442fdf97a9c5a0f51
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2025-10-23 10:14:18 +0900

    add: AutoRoterを導入しました

---
+++ lib/router/app_router.dart
@@ -0,0 +1,20 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterkaigi_2025_booth_game/title_view.dart';
+import 'package:flutterkaigi_2025_booth_game/game_view.dart';
+import 'package:flutterkaigi_2025_booth_game/result_view.dart';
+
+part 'app_router.gr.dart';
+
+@AutoRouterConfig()
+class AppRouter extends _$AppRouter {
+  @override
+  RouteType get defaultRouteType => const RouteType.adaptive();
+
+  @override
+  List<AutoRoute> get routes => [
+    AutoRoute(page: TitleRoute.page, path: '/', initial: true),
+    AutoRoute(page: GameRoute.page, path: '/game'),
+    AutoRoute(page: ResultRoute.page, path: '/result'),
+  ];
+}
